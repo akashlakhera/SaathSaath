72efeff74662d19feb0edfb64d53d9ad9daa5844
Aditya Kaushik <adityakaushik471@gmail.com>
2017-12-03 01:28:16 +0530

    Fix camera image selection

--- app/src/main/java/com/example/android/saathsaath/MainActivity.java
+++ app/src/main/java/com/example/android/saathsaath/MainActivity.java
@@ -5,12 +5,20 @@ import android.icu.lang.UProperty;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<String> issues;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        issues = new ArrayList<>();
+        issues.add("Harrasmant");
+        issues.add("Clealiness");
         startActivity(new Intent(MainActivity.this, UploadActivity.class));
+
     }
 }
